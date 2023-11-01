e00c154f261be924f76ec3a6a65c8ca344512e4a
zakaria mohamed <zakmi.moha@gmail.com>
2023-11-01 13:29:23 +0300

    Completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,11 +2,13 @@ import React from "react";
 
 function Item({ name, category }) {
   return (
+    <div>
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
       <button className="add">Add to Cart</button>
     </li>
+    </div>
   );
 }
 
